cf582cfabf37b5bbd4b1a0c1c64fbd78f42fd5ea
Prakash Yadava <prakash.kumar.yadava@gmail.com>
2025-01-11 19:56:36 +0530

    broiler-module

--- broiler/urls.py
+++ broiler/urls.py
@@ -2,6 +2,12 @@
 from django.urls import path
 from . import views
 
+from .views import BranchAPI, BranchTemplateView
+
 urlpatterns = [
     path('broiler/', views.broiler, name='broiler'),    
+    path('branches/', BranchTemplateView.as_view(), name='branch_list'),
+    path('branches/<int:pk>/', BranchAPI.as_view(), name='branch_edit'),  # For editing
+    path('branches/<int:pk>/delete/', BranchAPI.as_view(), name='branch_delete'),  # For deletion
+    
 ]
--- broiler/views.py
+++ broiler/views.py
@@ -1,8 +1,46 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.views import View
+from django.http import JsonResponse
+from django.views import View
+from .models import Branch
+import json
+
+
+from broiler.models import Branch
 
 
 
 
 def broiler(request):
     return render(request, 'broiler.html')
+
+
+class BranchTemplateView(View):
+    def get(self, request):
+        # Render the branch_template.html file
+        return render(request, 'branch.html')
+
+class BranchAPI(View):
+    def get(self, request):
+        branches = list(Branch.objects.values())
+        return JsonResponse(branches, safe=False)
+
+    def post(self, request):
+        data = json.loads(request.body)
+        Branch.objects.create(state=data['state'], branch_name=data['branch_name'])
+        return JsonResponse({'message': 'Branch created'}, status=201)
+
+    def put(self, request, id):
+        data = json.loads(request.body)
+        branch = Branch.objects.get(id=id)
+        branch.state = data['state']
+        branch.branch_name = data['branch_name']
+        branch.save()
+        return JsonResponse({'message': 'Branch updated'})
+
+    def delete(self, request, id):
+        branch = Branch.objects.get(id=id)
+        branch.delete()
+        return JsonResponse({'message': 'Branch deleted'})
