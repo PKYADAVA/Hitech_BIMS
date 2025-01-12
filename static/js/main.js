9f5ddfeea62c3d83d05d525c4980eb670bf575e0
vijay <vijay7458gautam@gmail.com>
2025-01-13 02:16:38 +0530

    mouseenter to open toggle

--- static/js/main.js
+++ static/js/main.js
@@ -1,38 +1,21 @@
 $(document).ready(function () {
-    // Initialize DataTable
-    $('#example').DataTable();
-    $('.dropdown-toggle').each(function () {
-        new bootstrap.Dropdown(this);
-    });
+  $('#example').DataTable();
 
-    $('.dropdown-submenu > a').on('click', function (e) {
-      var $subMenu = $(this).next('.dropdown-menu');
-  
-      $subMenu.toggleClass('show');
-  
-      e.stopPropagation();
-      e.preventDefault();
-    });
-  
-    $(document).on('click', function (e) {
-      if (!$(e.target).closest('.dropdown').length) {
-        $('.dropdown-menu').removeClass('show');
-      }
-    });
-  
-    $('.dropdown-submenu > a').on('click', function () {
-      var $currentMenu = $(this).next('.dropdown-menu');
-      $('.dropdown-submenu .dropdown-menu').not($currentMenu).removeClass('show');
-    });
+  $('.dropdown-toggle').each(function () {
+    new bootstrap.Dropdown(this);
+  });
 
-    $(document).on('click', function (e) {
-      if (!$(e.target).closest('.dropdown-attendance').length) {
-        $('.dropdown-attendance-menu').removeClass('show');
-      }
-    });
+  $('.dropdown-submenu').on('mouseenter', function () {
+    $(this).find('.dropdown-menu').addClass('show');
+  });
 
-    $('.dropdown-attendance-menu > a').on('click', function (e) {
-      var currentMenu = $(this).closest('.dropdown-attendance-menu');
-      $('.dropdown-attendance-menu').not(currentMenu).removeClass('show');
-    });
-  });      
+  $('.dropdown-submenu').on('mouseleave', function () {
+    $(this).find('.dropdown-menu').removeClass('show');
+  });
+
+  $(document).on('click', function (e) {
+    if (!$(e.target).closest('.dropdown').length) {
+      $('.dropdown-menu').removeClass('show');
+    }
+  });
+});
