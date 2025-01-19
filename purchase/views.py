f5b929ef3e22835eec05c3123f340c995d9e6b18
Prakash Yadava <prakash.kumar.yadava@gmail.com>
2025-01-19 22:51:45 +0530

    new-modules-add

--- purchase/views.py
+++ purchase/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+#pylint: disable=no-member
 
-# Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.http import Http404, JsonResponse
+from django.db.models import F
+from django.core.files.storage import default_storage
+from .models import Supplier
+import json
+
+
+
+@login_required()
+def purchase(request):
+    return render(request, "purchase.html")
--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+#pylint: disable=no-member
 
-# Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.http import Http404, JsonResponse
+from django.db.models import F
+from django.core.files.storage import default_storage
+
+import json
+
+
+
+@login_required()
+def sales(request):
+    return render(request, "sales.html")
