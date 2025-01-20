dd2fa6f10a9745507d399dcbfd244a8536238bc2
Prakash Yadava <prakash.kumar.yadava@gmail.com>
2025-01-20 21:27:22 +0530

    new-modules-add

--- account/urls.py
+++ account/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+   
+    path('account/', views.account, name='account'),
+    path('coa/', views.coa, name='coa'),
+    path('fin_year/', views.fin_year, name='fin_year'),
+    path('bank_cash/', views.bank_cash, name='bank_cash'),
+    path('schedule/', views.schedule, name='schedule'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+
+
+
+@login_required
+def account(request):
+    return render(request, "account.html")
+
+@login_required
+def coa(request):
+    return render(request, "coa.html")
+
+@login_required
+def fin_year(request):
+    return render(request, "fin_year.html")
+
+@login_required
+def bank_cash(request):
+    return render(request, "bank_cash.html")
+
+@login_required
+def schedule(request):
+    return render(request, "schedule.html")
