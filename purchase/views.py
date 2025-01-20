795e156dd7b8118d96d022dc43f16dc864e8b834
Prakash Yadava <prakash.kumar.yadava@gmail.com>
2025-01-20 22:20:57 +0530

    new-modules-add

--- purchase/views.py
+++ purchase/views.py
@@ -20,7 +20,48 @@ def purchase(request):
 
 @login_required()
 def supplier(request):
-    return render(request, "supplier.html")
+    states_and_union_territories = [
+        "Andhra Pradesh",
+        "Arunachal Pradesh",
+        "Assam",
+        "Bihar",
+        "Chhattisgarh",
+        "Goa",
+        "Gujarat",
+        "Haryana",
+        "Himachal Pradesh",
+        "Jharkhand",
+        "Karnataka",
+        "Kerala",
+        "Madhya Pradesh",
+        "Maharashtra",
+        "Manipur",
+        "Meghalaya",
+        "Mizoram",
+        "Nagaland",
+        "Odisha",
+        "Punjab",
+        "Rajasthan",
+        "Sikkim",
+        "Tamil Nadu",
+        "Telangana",
+        "Tripura",
+        "Uttar Pradesh",
+        "Uttarakhand",
+        "West Bengal",
+        "Andaman and Nicobar Islands",
+        "Chandigarh",
+        "Dadra and Nagar Haveli and Daman and Diu",
+        "Delhi",
+        "Jammu and Kashmir",
+        "Ladakh",
+        "Lakshadweep",
+        "Puducherry",
+    ]
+
+    # Pass the data as context
+    context = {"states_and_union_territories": states_and_union_territories}
+    return render(request, "supplier.html", context)
 
 
 
