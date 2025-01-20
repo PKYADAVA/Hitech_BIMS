c724bb17597671d463cce7c4b40139e65ba79235
Prakash Yadava <prakash.kumar.yadava@gmail.com>
2025-01-20 21:44:07 +0530

    new-modules-add

--- purchase/urls.py
+++ purchase/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path("supplier/", views.supplier, name="supplier"),
     path("terms/", views.terms, name="terms"),
     path("vendor_groups/", views.vendor_groups, name="vendor_groups"),
+    path('vendor_group/', views.VendorGroupAPI.as_view(), name='vendor_group_list'),
+    path('vendor_group/create/', views.VendorGroupAPI.as_view(), name='vendor_group_create'),
+    path('vendor_group/update/<int:id>/', views.VendorGroupAPI.as_view(), name='vendor_group_update'),
+    path('vendor_group/delete/<int:id>/', views.VendorGroupAPI.as_view(), name='vendor_group_delete'),
     path("tax_master/", views.tax_master, name="tax_master"),
 ]
