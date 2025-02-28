274cd01451dc88ed00c844808dd603354c3129ae
PRABHAT KUMAR YADAVA <109432776+PKYADAVA@users.noreply.github.com>
2025-02-28 13:09:15 +0530

    Update settings.py

--- Hitech_BIMS/settings.py
+++ Hitech_BIMS/settings.py
@@ -180,6 +180,8 @@ CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 
+print(os.getenv("REDIS_URL")
+
 
 LOGGING = {
     "version": 1,
