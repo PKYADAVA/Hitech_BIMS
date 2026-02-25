6bb3229085f58ffe3739cd827859472c733db166
PRABHAT KUMAR YADAVA <109432776+PKYADAVA@users.noreply.github.com>
2026-02-25 17:36:19 +0530

    Update settings.py

--- Hitech_BIMS/settings.py
+++ Hitech_BIMS/settings.py
@@ -88,12 +88,22 @@ WSGI_APPLICATION = "Hitech_BIMS.wsgi.application"
 
 # Database configuration
 if DEVELOPMENT_MODE:
+    # DATABASES = {
+    #     "default": {
+    #         "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.postgresql"),
+    #         "NAME": os.getenv("DB_NAME"),
+    #         "USER": os.getenv("DB_USER"),
+    #         "PASSWORD": os.getenv("DB_PASSWORD"),
+    #         "HOST": os.getenv("DB_HOST", "localhost"),
+    #         "PORT": os.getenv("DB_PORT", "5432"),
+    #     }
+    # }
     DATABASES = {
         "default": {
             "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.postgresql"),
-            "NAME": os.getenv("DB_NAME"),
-            "USER": os.getenv("DB_USER"),
-            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "NAME": "django_db",
+            "USER": "django_user",
+            "PASSWORD": "Prabhat@2207",
             "HOST": os.getenv("DB_HOST", "localhost"),
             "PORT": os.getenv("DB_PORT", "5432"),
         }
